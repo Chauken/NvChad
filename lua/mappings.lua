501679924634aaef11374e47a5e2a40a0d679148
Martin <martin.bilger@relyit.se>
2024-11-04 23:29:16 +0100

    remove alt h for terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+local nomap = vim.keymap.del
+nomap("n", "<A-h>")  -- remove normal mode binding
+nomap("t", "<A-h>")  -- remove terminal mode binding
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
